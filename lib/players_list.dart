081deda09647c14c6119a4da96559144c58c157a
Andrei Boyanov <andrei.boyanov@gmail.com>
2018-05-12 16:59:27 +0200

    Players selectable in the players list

--- lib/players_list.dart
+++ lib/players_list.dart
@@ -7,7 +7,6 @@ import 'player_details.dart';
 import 'image_tools.dart';
 
 class PlayersList extends StatefulWidget {
-
   PlayersList();
 
   @override
@@ -35,11 +34,8 @@ class _PlayersListState extends State<PlayersList> {
     return new EndlessList(_buildPlayerItem, _findPlayers);
   }
 
-  _findPlayers({filter, start, count}) =>
-      playerService.findPlayers(
-          filter: {"name": filter},
-          start: start,
-          max: count == null ? 20 : count);
+  _findPlayers({filter, start, count}) => playerService.findPlayers(
+      filter: {"name": filter}, start: start, max: count == null ? 20 : count);
 
   _buildPlayerItem(BuildContext context, Player player) =>
       new PlayerItemWidget(player);
@@ -57,55 +53,54 @@ class PlayerItemWidget extends StatefulWidget {
 }
 
 class _PlayerItemWidgetState extends State<PlayerItemWidget> {
+  bool selected = false;
+
   @override
   build(BuildContext context) {
     return new Container(
         margin: const EdgeInsets.only(top: 8.0),
         child: ListTile(
-            leading: new CircleAvatar(
-                backgroundImage: getImageProvider(widget.player.photoUrl)
+          leading: new IconButton(
+            icon: new CircleAvatar(
+              backgroundImage: getImageProvider(widget.player.photoUrl),
             ),
-            title: new Container(
-                child: new Text(""
-                    "${widget.player.firstName} "
-                    "${widget.player.lastName} ",
-                    style: new TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.black,
-                    )
-                ),
-                padding: const EdgeInsets.only(bottom: 8.0)),
-            subtitle: new Container(
-                child: new Text(""
-                    "${widget.player.id} "
-                    "${widget.player.singleRanking} "
-                    "${widget.player.clubName} (${widget.player.clubId})"
-                )
-            ),
-            trailing: new IconButton(
-              onPressed: () =>
-                  setState(() {
-                    widget.player.toggleFavorited();
-                    PlayersList.of(context).onPlayerChanged(widget.player);
-                  }),
-              icon: new Icon(widget.player.isFavorited
-                  ? Icons.favorite
-                  : Icons.favorite_border,
-                  size: 16.0),
-            ),
-            onTap: () =>
-                Navigator.push(context, new MaterialPageRoute(
-                    builder: (context) => new PlayerDetails(widget.player)
-                )),
+            onPressed: () => setState(() => selected = !selected),
+          ),
+          title: new Container(
+              child: new Text(
+                  ""
+                  "${widget.player.firstName} "
+                  "${widget.player.lastName} ",
+                  style: new TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black,
+                  )),
+              padding: const EdgeInsets.only(bottom: 8.0)),
+          subtitle: new Container(
+              child: new Text(""
+                  "${widget.player.id} "
+                  "${widget.player.singleRanking} "
+                  "${widget.player.clubName} (${widget.player.clubId})")),
+          trailing: new IconButton(
+            onPressed: () => setState(() {
+                  widget.player.toggleFavorited();
+                  PlayersList.of(context).onPlayerChanged(widget.player);
+                }),
+            icon: new Icon(
+                widget.player.isFavorited
+                    ? Icons.favorite
+                    : Icons.favorite_border,
+                size: 16.0),
+          ),
+          selected: selected,
+          onTap: () => Navigator.push(
+                context,
+                new MaterialPageRoute(
+                    builder: (context) => new PlayerDetails(widget.player)),
+              ),
         ),
         decoration: new BoxDecoration(
             border: new Border(
-                bottom: new BorderSide(
-                    color: Colors.black12,
-                    width: 0.5)
-            )
-        )
-    );
+                bottom: new BorderSide(color: Colors.black12, width: 0.5))));
   }
 }
-
